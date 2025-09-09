d3150595c64cc031f3c5e9efcef7237fdf3c367c
Adriano Lima <jink_adriano@hotmail.com>
2025-09-08 21:13:17 -0300

    aula atualizada

--- backend/src/main/java/br/edu/unichristus/backend/controller/ExampleController.java
+++ backend/src/main/java/br/edu/unichristus/backend/controller/ExampleController.java
@@ -1,18 +1,37 @@
 package br.edu.unichristus.backend.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
-@RequestMapping("/example")
+@RequestMapping("/person")
 public class ExampleController {
 
-    @GetMapping("/sum")
-    public double sum(@RequestParam(name="n1")  double n1,
-                      @RequestParam(name="n2") double n2){
-        return n1 + n2;
+    private List<Pessoa> pessoas = new ArrayList<>();
+
+    @PostMapping
+    public Pessoa save(@RequestBody Pessoa pessoa){
+        if(pessoas.isEmpty()){
+            pessoa.setId(1L);
+        }else{
+            int ultimaPosicao = pessoas.size() - 1;
+            pessoa.setId(pessoas.get(ultimaPosicao).getId() + 1);
+        }
+        pessoas.add(pessoa);
+        return pessoa;
     }
+
+    @GetMapping("/{id}")
+    public Pessoa findById(@PathVariable(name = "id") Long id){
+        for(Pessoa pessoa : pessoas){
+            if(pessoa.getId().equals(id))
+                return pessoa;
+        }
+        return new Pessoa();
+    }
+
+
 
 }
